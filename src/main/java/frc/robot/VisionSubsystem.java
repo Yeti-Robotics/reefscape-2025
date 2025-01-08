2c7bacba0e56ae79ffbed1f2b39f3313fba65c1e
DL <thedanyaliu@gmail.com>
2025-01-07 20:05:47 -0500

    Added LimelightHelpers and began VisionSubsystem. Created methods, but do not know where to use pose estimation and Megatag2.

---
+++ src/main/java/frc/robot/VisionSubsystem.java
@@ -0,0 +1,25 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.*;
+
+public class VisionSubsystem extends SubsystemBase {
+    public class VisionConstants{
+        public static final String PICK_LIMELIGHT = "pick limelight";
+        public static final String PLACE_LIMELIGHT = "place limelight";
+
+    }
+
+    public double getX() {
+        return LimelightHelpers.getTX(VisionConstants.PLACE_LIMELIGHT);
+    }
+
+    public double getY() {
+        return LimelightHelpers.getTY(VisionConstants.PLACE_LIMELIGHT);
+    }
+
+    public LimelightHelpers.LimelightResults getTargetingResults() {
+        return LimelightHelpers.getLatestResults(VisionConstants.PLACE_LIMELIGHT);
+    }
+
+
+}
