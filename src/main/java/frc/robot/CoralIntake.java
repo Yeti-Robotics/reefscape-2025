5f09ba36dc1c78a227866eabd3f383d1155321f9
DL <thedanyaliu@gmail.com>
2025-01-11 09:22:18 -0500

    Made constants for backward and forward speeds.

--- src/main/java/frc/robot/CoralIntake.java
+++ src/main/java/frc/robot/CoralIntake.java
@@ -12,22 +12,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class CoralIntake extends SubsystemBase {
     private final TalonFX claw;
 
-    public static class IntakeConstants {
+    private static class IntakeConfigs {
         public static final int CLAW_ID = 9;
         public static final InvertedValue CLAW_INVERSION = InvertedValue.Clockwise_Positive;
         public static final NeutralModeValue CLAW_NEUTRAL_MODE = NeutralModeValue.Coast;
         public static final double POSITION_STATUS_FRAME = 0.05;
         public static final double VELOCITY_STATUS_FRAME = 0.01;
-    }
 
-    // Constructor that sets configs for claw
+    }
+    public static final double FORWARD_SPEED = 1;
+    public static final double BACKWARD_SPEED = -1;
+
     public CoralIntake() {
-        claw = new TalonFX(IntakeConstants.CLAW_ID, RIO_BUS);
+        claw = new TalonFX(IntakeConfigs.CLAW_ID, RIO_BUS);
 
         var clawConfigurator = claw.getConfigurator();
         var configs = new TalonFXConfiguration();
-        configs.MotorOutput.Inverted = IntakeConstants.CLAW_INVERSION;
-        configs.MotorOutput.NeutralMode = IntakeConstants.CLAW_NEUTRAL_MODE;
+        configs.MotorOutput.Inverted = IntakeConfigs.CLAW_INVERSION;
+        configs.MotorOutput.NeutralMode = IntakeConfigs.CLAW_NEUTRAL_MODE;
         clawConfigurator.apply(configs);
     }
 
@@ -42,5 +44,13 @@ public class CoralIntake extends SubsystemBase {
     // Spins claw at the set speed, can be both in and out depending on speed
     public Command spinClaw(double speed) {
         return startEnd(() -> setClawSpeed(speed), this::stopClaw);
+    }
+
+    public Command spinClawForward(){
+        return spinClaw(FORWARD_SPEED);
+    }
+
+    public Command spinClawBackward(){
+        return spinClaw(BACKWARD_SPEED);
     }
 }
