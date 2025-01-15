d10e168cf8e222b563aa648fd17bcab7c9b8b7b5
DL <thedanyaliu@gmail.com>
2025-01-14 19:00:31 -0500

    Protected configs of the intakes.

--- src/main/java/frc/robot/subsystems/algae/AlgaeConfigs.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeConfigs.java
@@ -4,9 +4,9 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 class AlgaeConfigs {
-        protected static final int ROLLER_ID = 8;
-        protected static final InvertedValue ROLLER_INVERSION = InvertedValue.Clockwise_Positive;
-        protected static final NeutralModeValue ROLLER_NEUTRAL_MODE = NeutralModeValue.Coast;
-        protected static final double POSITION_STATUS_FRAME = 0.05;
-        protected static final double VELOCITY_STATUS_FRAME = 0.01;
+    static final int ROLLER_ID = 8;
+    static final InvertedValue ROLLER_INVERSION = InvertedValue.Clockwise_Positive;
+    static final NeutralModeValue ROLLER_NEUTRAL_MODE = NeutralModeValue.Coast;
+    static final double POSITION_STATUS_FRAME = 0.05;
+    static final double VELOCITY_STATUS_FRAME = 0.01;
 }
--- src/main/java/frc/robot/subsystems/coral/CoralConfigs.java
+++ src/main/java/frc/robot/subsystems/coral/CoralConfigs.java
@@ -4,9 +4,9 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 class CoralConfigs {
-        protected static final int CLAW_ID = 9;
-        protected static final InvertedValue CLAW_INVERSION = InvertedValue.Clockwise_Positive;
-        protected static final NeutralModeValue CLAW_NEUTRAL_MODE = NeutralModeValue.Coast;
-        protected static final double POSITION_STATUS_FRAME = 0.05;
-        protected static final double VELOCITY_STATUS_FRAME = 0.01;
+    static final int CLAW_ID = 9;
+    static final InvertedValue CLAW_INVERSION = InvertedValue.Clockwise_Positive;
+    static final NeutralModeValue CLAW_NEUTRAL_MODE = NeutralModeValue.Coast;
+    static final double POSITION_STATUS_FRAME = 0.05;
+    static final double VELOCITY_STATUS_FRAME = 0.01;
 }
