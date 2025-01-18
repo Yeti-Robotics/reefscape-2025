ae4633c9f6fb2a4992449103e17d2a80d9a7290a
taran-duba <tkduba19@gmail.com>
2025-01-18 10:35:05 -0500

    fixed gradle violations

--- src/main/java/frc/robot/subsystems/wrist/WristSubsystem.java
+++ src/main/java/frc/robot/subsystems/wrist/WristSubsystem.java
@@ -1,12 +1,9 @@
 package frc.robot.subsystems.wrist;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.MotionMagicExpoVoltage;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
