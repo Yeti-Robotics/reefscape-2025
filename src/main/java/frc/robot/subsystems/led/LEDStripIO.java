fa039a54ce8a6849051370855482bcb39d10a27a
qwertycloudhub <vihaansden@gmail.com>
2025-01-25 13:30:25 -0500

    LED setup

---
+++ src/main/java/frc/robot/subsystems/led/LEDStripIO.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems.led;
+
+public interface LEDStripIO {
+    void ledStripOff();
+
+    void idleBlueAlliance();
+
+    void idleRedAlliance();
+
+    void coralIntakeAnimation();
+
+    void coralStowAnimation();
+
+    void algaeIntakeAnimation();
+
+    void nick();
+
+    void progressBar();
+}
