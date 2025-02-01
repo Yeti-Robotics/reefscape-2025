23f57e6ddff8ae961c2c95fb36fdb1d05c8c6893
yetirobotics <yetiroboticsengineering@gmail.com>
2025-02-01 13:47:25 -0500

    fix settings and test path

--- src/main/java/frc/robot/subsystems/drivetrain/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/CommandSwerveDrivetrain.java
@@ -7,8 +7,14 @@ import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -19,7 +25,9 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.subsystems.drivetrain.TunerConstants.TunerSwerveDrivetrain;
+import java.io.IOException;
 import java.util.function.Supplier;
+import org.json.simple.parser.ParseException;
 
 /**
  * Class that extends the Phoenix 6 SwerveDrivetrain class and implements Subsystem so it can easily
@@ -44,6 +52,37 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
             new SwerveRequest.SysIdSwerveSteerGains();
     private final SwerveRequest.SysIdSwerveRotation m_rotationCharacterization =
             new SwerveRequest.SysIdSwerveRotation();
+    private final SwerveRequest.ApplyRobotSpeeds AutoReq = new SwerveRequest.ApplyRobotSpeeds();
+
+    RobotConfig config;
+
+    {
+        try {
+            config = RobotConfig.fromGUISettings();
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException(e);
+        }
+        AutoBuilder.configure(
+                () -> this.getState().Pose,
+                this::resetPose,
+                this::getChassisSpeeds,
+                (ChassisSpeeds speeds) -> this.setControl(AutoReq.withSpeeds(speeds)),
+                new PPHolonomicDriveController(
+                        new PIDConstants(5.0, 0.0, 0.0), new PIDConstants(5.0, 0.0, 0.0)),
+                config,
+                () -> {
+                    var alliance = DriverStation.getAlliance();
+                    return alliance.filter(value -> value == Alliance.Red).isPresent();
+                },
+                this);
+    }
+
+    protected SwerveDriveKinematics m_kinematics =
+            new SwerveDriveKinematics(config.moduleLocations);
+
+    public ChassisSpeeds getChassisSpeeds() {
+        return m_kinematics.toChassisSpeeds(getState().ModuleStates);
+    }
 
     /* SysId routine for characterizing translation. This is used to find PID gains for the drive motors. */
     private final SysIdRoutine m_sysIdRoutineTranslation =
--- src/main/java/frc/robot/util/AutoConstants.java
+++ src/main/java/frc/robot/util/AutoConstants.java
@@ -7,8 +7,5 @@ public class AutoConstants {
     public static final LinearVelocity MAX_SPEED = TunerConstants.kSpeedAt12Volts;
     public static final LinearVelocity MAX_ACCEL = TunerConstants.kSpeedAt12Volts;
 
-
-
-    public enum AutoMode{
-    }
+    public enum AutoMode {}
 }
