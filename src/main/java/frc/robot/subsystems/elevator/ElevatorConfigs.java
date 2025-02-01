08b5c6350e43023d662b78946295b767f33f358c
Prathik Saduneni <151557515+prthik@users.noreply.github.com>
2025-02-01 15:14:15 -0500

    resolved requested changes for elevator

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConfigs.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConfigs.java
@@ -1,35 +1,25 @@
 package frc.robot.subsystems.elevator;
 
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 
 class ElevatorConfigs {
     static final int primaryElevatorMotorID = 42;
     static final int secondaryElevatorMotorID = 15;
     static final int magSwitchID = 0; // placeholder
 
-    static final double Elevator_kP = 2.4; //placeholder
-    static final double Elevator_kI = 0; //placeholder
-    static final double Elevator_kD = 0.1; //placeholder
+    static final TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
 
-    static Slot0Configs slot0configs = new Slot0Configs()
-            .withKP(Elevator_kP)
-            .withKI(Elevator_kI)
-            .withKD(Elevator_kD);
+    static {
+        Slot0Configs slot0configs = new Slot0Configs()
+                .withKP(2.4) //placeholder
+                .withKI(0) //placeholder
+                .withKD(0.1); //placeholder
 
+        var motionMagicConfigs = talonFXConfigs.MotionMagic;
+        motionMagicConfigs.MotionMagicCruiseVelocity = 1; //placeholder
+        motionMagicConfigs.MotionMagicAcceleration = 1; //placeholder
+        motionMagicConfigs.MotionMagicJerk = 1; //placeholder
 
-    public enum ElevatorPosition {
-        BOTTOM(0.0), // placeholder
-        MIDDLE(24.0), // placeholder
-        TOP(40.0); //placeholser
-
-        private final double height;
-
-        ElevatorPosition(double height) {
-            this.height = height;
-        }
-
-        public double getHeight() {
-            return height;
-        }
     }
 }
--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -1,76 +1,54 @@
 package frc.robot.subsystems.elevator;
 
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.constants.Constants.RIO_BUS;
+import static frc.robot.subsystems.elevator.ElevatorConfigs.talonFXConfigs;
 
 public class ElevatorSubsystem extends SubsystemBase {
-    // Motors
     private final TalonFX primaryElevatorMotor;
     private final TalonFX secondaryElevatorMotor;
-
-    // Sensors
     private final DigitalInput magSwitch;
-
     public final MotionMagicVoltage magicRequest;
 
-    // Constants
-    private static final int TICKS_PER_INCH = 1000;
-    private static final int STAGE_RATIO = 2;
-
-
     public ElevatorSubsystem() {
-        primaryElevatorMotor = new TalonFX(ElevatorConfigs.primaryElevatorMotorID, "canivoreBUS");
-        secondaryElevatorMotor = new TalonFX(ElevatorConfigs.secondaryElevatorMotorID, "canivoreBUS");
-
-        var talonFXConfigs = new TalonFXConfiguration();
-        talonFXConfigs.Slot0.kP = ElevatorConfigs.Elevator_kP;
-        talonFXConfigs.Slot0.kI = ElevatorConfigs.Elevator_kI;
-        talonFXConfigs.Slot0.kD = ElevatorConfigs.Elevator_kD;
+        primaryElevatorMotor = new TalonFX(ElevatorConfigs.primaryElevatorMotorID, RIO_BUS);
+        secondaryElevatorMotor = new TalonFX(ElevatorConfigs.secondaryElevatorMotorID, RIO_BUS);
 
         primaryElevatorMotor.getConfigurator().apply(talonFXConfigs);
 
         //Secondary motor follows the primary motor
         secondaryElevatorMotor.setControl(new Follower(ElevatorConfigs.primaryElevatorMotorID, true));
-
-        // Initialize magnetic switch
         magSwitch = new DigitalInput(ElevatorConfigs.magSwitchID);
-
-        // Initialize Motion Magic request
         magicRequest = new MotionMagicVoltage(0);
-
-        var motionMagicConfigs = talonFXConfigs.MotionMagic;
-        motionMagicConfigs.MotionMagicCruiseVelocity = 1; //placeholder
-        motionMagicConfigs.MotionMagicAcceleration = 1; //placeholder
-        motionMagicConfigs.MotionMagicJerk = 1; //placeholder
     }
-
-    // Sets elevator position to a specific height
-    public void setPosition(ElevatorConfigs.ElevatorPosition position) {
-        double targetTicks = position.getHeight() * TICKS_PER_INCH / STAGE_RATIO;
-        primaryElevatorMotor.setControl(magicRequest.withPosition(targetTicks));
+    public void setPosition(ElevatorPosition position) {
+        primaryElevatorMotor.setControl(magicRequest.withPosition(position.getHeight()));
     }
-
-    // Stops the elevator
     public void stop() {
         primaryElevatorMotor.stopMotor();
     }
-
-    // Checks magSwitch status
     public boolean getMagSwitch() {
         return magSwitch.get();
     }
-
-    public Command moveUp(){
-        return runOnce(() -> setPosition(ElevatorConfigs.ElevatorPosition.TOP));
+    public Command moveUpLevel1(){
+        return runOnce(() -> setPosition(ElevatorPosition.LEVEL1));
     }
-
+    public Command moveUpLevel2(){
+        return runOnce(() -> setPosition(ElevatorPosition.LEVEL2));
+    }
+    public Command moveUpLevel3(){
+        return runOnce(() -> setPosition(ElevatorPosition.LEVEL3));
+    }
+    public Command moveUpLevel4(){
+        return runOnce(() -> setPosition(ElevatorPosition.LEVEL4));
+    }
     public Command moveDown(){
-        return runOnce(() -> setPosition(ElevatorConfigs.ElevatorPosition.BOTTOM));
+        return runOnce(() -> setPosition(ElevatorPosition.BOTTOM));
     }
 
     @Override
