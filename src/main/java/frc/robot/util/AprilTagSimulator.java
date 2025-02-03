4d9e4de2d6a0171043c510435829f2957db853fc
Sam Perlmutter <sam.perlmutter227@gmail.com>
2025-02-02 21:54:12 -0500

    Created builder class for april tag camera simulation

---
+++ src/main/java/frc/robot/util/AprilTagSimulator.java
@@ -0,0 +1,33 @@
+package frc.robot.util;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.photonvision.simulation.VisionSystemSim;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AprilTagSimulator extends SubsystemBase {
+    VisionSystemSim visionSim;
+    List<AprilTagCamSim> aprilTagCamSims;
+
+    public AprilTagSimulator() {
+        visionSim = new VisionSystemSim("main");
+        aprilTagCamSims = new ArrayList<>();
+
+        visionSim.addAprilTags(AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField));
+    }
+
+    public void addCamera(AprilTagCamSim aprilTagCamSim) {
+        visionSim.addCamera(aprilTagCamSim.getCameraSim(), aprilTagCamSim.getTransform());
+        aprilTagCamSims.add(aprilTagCamSim);
+    }
+
+    public void update(Pose2d pose) {
+        visionSim.update(pose);
+        aprilTagCamSims.forEach(AprilTagCamSim::publishSeenTags);
+    }
+
+}
