741caec3d113ada22213246944509ceb2524dae3
taran-duba <72531052+taran-duba@users.noreply.github.com>
2025-02-04 19:20:31 -0500

    broken code :sob:

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.led.LEDSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the methods corresponding to
@@ -20,6 +21,7 @@ public class Robot extends TimedRobot {
     private Command autonomousCommand;
 
     private RobotContainer robotContainer;
+    private LEDSubsystem leds;
 
     /**
      * This method is run when the robot is first started up and should be used for any
@@ -30,6 +32,7 @@ public class Robot extends TimedRobot {
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
         robotContainer = new RobotContainer();
+        leds = new LEDSubsystem();
     }
 
     /**
@@ -50,7 +53,10 @@ public class Robot extends TimedRobot {
 
     /** This method is called once each time the robot enters Disabled mode. */
     @Override
-    public void disabledInit() {}
+    public void disabledInit() {
+        leds.setAnimation(LEDSubsystem.Events.OFF);
+        leds.setAnimation(LEDSubsystem.Events.PROGRESSBAR);
+    }
 
     @Override
     public void disabledPeriodic() {}
@@ -85,7 +91,9 @@ public class Robot extends TimedRobot {
 
     /** This method is called periodically during operator control. */
     @Override
-    public void teleopPeriodic() {}
+    public void teleopPeriodic() {
+        leds.setAnimation(LEDSubsystem.Events.IDLETELEOP);
+    }
 
     @Override
     public void testInit() {
--- src/main/java/frc/robot/subsystems/led/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/led/LEDSubsystem.java
@@ -4,19 +4,12 @@ import com.ctre.phoenix.led.*;
 import com.ctre.phoenix.led.CANdle.LEDStripType;
 import com.ctre.phoenix.led.CANdle.VBatOutputMode;
 import com.ctre.phoenix.led.LarsonAnimation.BounceMode;
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Robot;
 import frc.robot.constants.Constants;
 
 public class LEDSubsystem extends SubsystemBase {
     public final CANdle candle = new CANdle(0, Constants.RIO_BUS);
-    public final AddressableLED led = new AddressableLED(0);
-    public final AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(36);
     private Animation toAnimate = null;
     public int ledCount = 39;
     public int ledOffset = 8;
@@ -34,81 +27,71 @@ public class LEDSubsystem extends SubsystemBase {
     }
 
     public LEDSubsystem() {
-        //        this.joystick = joystick;
-        led.setLength(ledBuffer.getLength());
         CANdleConfiguration configAll = new CANdleConfiguration();
         configAll.statusLedOffWhenActive = false;
         configAll.disableWhenLOS = false;
         configAll.stripType = LEDStripType.RGB;
         configAll.brightnessScalar = 0.5;
         configAll.vBatOutputMode = VBatOutputMode.On;
-        candle.configAllSettings(configAll, 1);
-        //        System.out.println("Candle Output");
-        //        System.out.println(candle.setLEDs(8, 255, 8, 8, 0, 14));
-        //        candle.setLEDs(220, 0, 0, 22, 22, 17);
-        new Trigger(DriverStation::isDisabled).onFalse(Commands.print("led off"));
+        candle.configAllSettings(configAll, 100);
     }
 
-    public void changeAnimation(Events animation) {
+    public void setAnimation(Events animation) {
         switch (animation) {
             case NICK:
                 toAnimate = new StrobeAnimation(255, 0, 25, 0, 4, ledCount);
+                candle.animate(toAnimate);
                 break;
             case ALGAEINTAKE:
                 toAnimate =
                         new LarsonAnimation(
                                 79, 165, 181, 0, 0.3, ledCount, BounceMode.Front, 3, ledOffset);
+                candle.animate(toAnimate);
                 break;
             case CORALINTAKE:
                 toAnimate =
                         new LarsonAnimation(
                                 255, 255, 255, 255, 0.3, ledCount, BounceMode.Front, 7, ledOffset);
+                candle.animate(toAnimate);
                 break;
             case CORALSTOWED:
                 toAnimate = new StrobeAnimation(255, 255, 255, 255, 3, ledCount);
+                candle.animate(toAnimate);
                 break;
             case IDLETELEOP:
                 if (Robot.isRedAlliance()) {
                     toAnimate =
                             new LarsonAnimation(
                                     255, 0, 0, 0, 0.3, ledCount, BounceMode.Front, 3, ledOffset);
+                    candle.animate(toAnimate);
                 } else {
                     toAnimate =
                             new LarsonAnimation(
                                     84, 229, 182, 0, 0.3, ledCount, BounceMode.Front, 3, ledOffset);
+                    candle.animate(toAnimate);
                 }
                 break;
             case PROGRESSBAR:
-                //                ProgressBar progressBar = new ProgressBar();
-                //                progressBar.setProgress(ProgressBar.ProgressBarPercents.FIFTY);
+                ProgressBar progressBar = new ProgressBar();
+                progressBar.setProgress(ProgressBar.ProgressBarPercents.FULL);
                 break;
             case FLAME:
                 toAnimate = new FireAnimation(1, 0.2, 15, 0.5, 0.2, false, 8);
+                candle.animate(toAnimate);
                 break;
             case OFF:
                 toAnimate = new StrobeAnimation(0, 0, 0, 0, 4, ledCount);
+                candle.animate(toAnimate);
+                break;
             case LOS:
                 if (Robot.isRedAlliance()) {
                     toAnimate = new StrobeAnimation(255, 0, 0, 0, 0.2, ledCount);
+                    candle.animate(toAnimate);
                 } else {
                     toAnimate = new StrobeAnimation(84, 229, 182, 0, 0.2, ledCount);
+                    candle.animate(toAnimate);
                 }
-        }
-    }
-
-    @Override
-    public void periodic() {
-        //        candle.setLEDs(
-        //                (int) (joystick.getLeftTriggerAxis() * 255),
-        //                (int) (joystick.getRightTriggerAxis() * 255),
-        //                (int) (joystick.getLeftX() * 255));
-        //        candle.setLEDs(100, 100, 210);
-        if (DriverStation.isDisabled()) {
-            candle.clearAnimation(0);
-            candle.setLEDs(255, 0, 0);
-        } else {
-            changeAnimation(Events.IDLETELEOP);
-            candle.animate(toAnimate);
+                break;
         }
     }
 }
