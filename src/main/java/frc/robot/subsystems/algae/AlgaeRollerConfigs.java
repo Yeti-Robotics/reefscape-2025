06c04a458b2a776e1080ec72342c7908c74029f0
yetirobotics <yetiroboticsengineering@gmail.com>
2025-02-08 11:00:54 -0500

    renamed algae stuff

--- src/main/java/frc/robot/subsystems/algae/AlgaeRollerConfigs.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeRollerConfigs.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems.algae;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-class AlgaeConfigs {
+class AlgaeRollerConfigs {
     static final int ROLLER_ID = 8;
     static final InvertedValue ROLLER_INVERSION = InvertedValue.Clockwise_Positive;
     static final NeutralModeValue ROLLER_NEUTRAL_MODE = NeutralModeValue.Coast;
