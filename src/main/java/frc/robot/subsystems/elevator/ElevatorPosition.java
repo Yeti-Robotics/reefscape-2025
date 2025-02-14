0411a27b205b3c8d0a185d56920fb3eff6f833cf
MandarChitale123 <mandarchitale87784@gmail.com>
2025-02-13 19:20:59 -0500

    Resolved

--- src/main/java/frc/robot/subsystems/elevator/ElevatorPosition.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorPosition.java
@@ -2,10 +2,10 @@ package frc.robot.subsystems.elevator;
 
 public enum ElevatorPosition {
     BOTTOM(0.0), // placeholder
-    LEVEL1(0.0), // placeholder
-    LEVEL2(0.0), // placeholder
-    LEVEL3(0.0), // placeholder
-    LEVEL4(0.0); // placeholder
+    LEVEL1(8.0), // placeholder
+    LEVEL2(16.0), // placeholder
+    LEVEL3(24.0), // placeholder
+    LEVEL4(40.0); // placeholder
 
     private final double height;
 
