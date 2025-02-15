c1c01ff6a283160f690a132a88d832dc1f6e36d1
yetirobotics <yetiroboticsengineering@gmail.com>
2025-02-15 16:54:40 -0500

    added elevator position (safe intaking)

--- src/main/java/frc/robot/subsystems/elevator/ElevatorPosition.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorPosition.java
@@ -5,7 +5,9 @@ public enum ElevatorPosition {
     LEVEL1(8.0), // placeholder
     LEVEL2(16.0), // placeholder
     LEVEL3(24.0), // placeholder
-    LEVEL4(40.0); // placeholder
+    LEVEL4(40.0),
+    INTAKE(1.65);
+    //for clearance at least 1.86
 
     private final double height;
 
