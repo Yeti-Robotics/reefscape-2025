9a7d01ba2254e6a06cf845ae46133cc56c7512b7
Sam Perlmutter <sam.perlmutter227@gmail.com>
2025-02-17 19:41:28 -0500

    Group tuning values together

--- src/main/java/frc/robot/util/sim/TalonFXLogger.java
+++ src/main/java/frc/robot/util/sim/TalonFXLogger.java
@@ -19,12 +19,13 @@ public class TalonFXLogger extends ClassSpecificLogger<TalonFX> {
         backend.log("Applied Rotor Velocity", talon.getAppliedRotorPolarity().getValueAsDouble());
         backend.log("Duty Cycle", talon.getDutyCycle().getValueAsDouble());
 
-        backend.log("Position", talon.getPosition().getValueAsDouble());
-        backend.log("Velocity", talon.getVelocity().getValueAsDouble());
-        backend.log("Acceleration", talon.getAcceleration().getValueAsDouble());
-        backend.log("Target Error", talon.getClosedLoopError().getValueAsDouble());
-        backend.log("Target Position", talon.getClosedLoopReference().getValueAsDouble());
-        backend.log("Target Velocity", talon.getClosedLoopReferenceSlope().getValueAsDouble());
+        backend.log("Tuning/Position", talon.getPosition().getValueAsDouble());
+        backend.log("Tuning/Velocity", talon.getVelocity().getValueAsDouble());
+        backend.log("Tuning/Acceleration", talon.getAcceleration().getValueAsDouble());
+        backend.log("Tuning/Target Error", talon.getClosedLoopError().getValueAsDouble());
+        backend.log("Tuning/Target Position", talon.getClosedLoopReference().getValueAsDouble());
+        backend.log(
+                "Tuning/Target Velocity", talon.getClosedLoopReferenceSlope().getValueAsDouble());
         backend.log("Closed Loop Output", talon.getClosedLoopOutput().getValueAsDouble());
         backend.log(
                 "Closed Loop P Output", talon.getClosedLoopProportionalOutput().getValueAsDouble());
