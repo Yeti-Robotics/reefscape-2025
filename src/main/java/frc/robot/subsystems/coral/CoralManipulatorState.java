f371209a9b942b5a1e08d55acab01c6db8f39990
yetirobotics <yetiroboticsengineering@gmail.com>
2025-03-08 18:56:59 -0500

    merged develop (and fixed that one annoying error)

--- src/main/java/frc/robot/subsystems/coral/CoralManipulatorState.java
+++ src/main/java/frc/robot/subsystems/coral/CoralManipulatorState.java
@@ -8,7 +8,8 @@ import frc.robot.subsystems.coral.wrist.WristPositions;
 public enum CoralManipulatorState {
     INTAKE_CORAL(
             ArmPosition.DOWN, ElevatorPosition.INTAKE, GrabberState.ROLL_IN, WristPositions.UNSAFE),
-    L1(ArmPosition.POS_L1, ElevatorPosition.POS_L1, GrabberState.ROLL_OUT, WristPositions.UNSAFE),
+    L1(ArmPosition.POS_L1, ElevatorPosition.POS_L1, GrabberState.OFF, WristPositions.UNSAFE),
+    SCORE_L1(ArmPosition.POS_L1, ElevatorPosition.POS_L1, GrabberState.ROLL_OUT, WristPositions.UNSAFE),
     L2(ArmPosition.POS_L2, ElevatorPosition.POS_L2, GrabberState.OFF, WristPositions.SAFE),
     SCORE_L2(
             ArmPosition.SCORE_L2,
