d34b9f7ab1c64c03bd6c6870c51ed5bb74f0ada9
DL <thedanyaliu@gmail.com>
2025-03-09 16:27:48 -0400

    moved auto commands into command folder

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -23,6 +23,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.AutoNamedCommands;
 import frc.robot.commands.ReefAlignCommand;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.coral.CoralManipulatorState;
--- src/main/java/frc/robot/commands/AutoNamedCommands.java
+++ src/main/java/frc/robot/commands/AutoNamedCommands.java
@@ -1,4 +1,4 @@
-package frc.robot;
+package frc.robot.commands;
 
 import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
