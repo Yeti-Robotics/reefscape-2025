92173165b6292c6e276e92b6cb66ddb13d6c53ab
yetirobotics <yetiroboticsengineering@gmail.com>
2025-03-22 08:22:59 -0400

    logged build date in robotinit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,10 +10,12 @@ import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.coral.CoralManipulatorState;
 import frc.robot.util.sim.PhysicsSim;
+import org.littletonrobotics.frc2025.BuildConstants;
 
 /**
  * The VM is configured to automatically run this class, and to call the methods corresponding to
@@ -37,6 +39,7 @@ public class Robot extends TimedRobot {
         DataLogManager.start();
         DriverStation.startDataLog(DataLogManager.getLog());
         Epilogue.bind(this);
+        SmartDashboard.putString("Build Constants", BuildConstants.BUILD_DATE);
     }
 
     /**
