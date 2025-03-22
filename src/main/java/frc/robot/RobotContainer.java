bb5b77a6ff6a00447145cdbec3fd6e3ae4215b90
yetirobotics <yetiroboticsengineering@gmail.com>
2025-03-22 19:09:03 -0400

    Update at 'Sat Mar 22 19:09:02 EDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -72,11 +72,11 @@ public class RobotContainer {
                             Units.inchesToMeters(22.5 - 7)),
                     new Rotation3d(0, Math.toRadians(15), Math.toRadians(-90)));
 
-        @Logged(name = "Vision/ScoreCam")
-        public final PhotonAprilTagSystem reefCam1;
+    @Logged(name = "Vision/ScoreCam")
+    public final PhotonAprilTagSystem reefCam1;
 
-        @Logged(name = "Vision/ClimbCam")
-        public final PhotonAprilTagSystem reefCam2;
+    @Logged(name = "Vision/ClimbCam")
+    public final PhotonAprilTagSystem reefCam2;
 
     @Logged(name = "CoralManipulator")
     final CoralManipulatorSystem coralManipulator;
@@ -103,37 +103,37 @@ public class RobotContainer {
         secondaryXboxController = new CommandXboxController(1);
         drivetrain = TunerConstants.createDrivetrain();
 
-                reefCam1 = new PhotonAprilTagSystem("ScoreCam", camTrans1, drivetrain);
-                reefCam2 = new PhotonAprilTagSystem("ClimbCam", camTrans2, drivetrain);
+        reefCam1 = new PhotonAprilTagSystem("ScoreCam", camTrans1, drivetrain);
+        reefCam2 = new PhotonAprilTagSystem("ClimbCam", camTrans2, drivetrain);
 
-                AprilTagCamSim simCam1 =
-                        AprilTagCamSimBuilder.newCamera()
-                                .withCameraName("ScoreCam")
-                                .withTransform(camTrans1)
-                                .build();
-                aprilTagCamSim.addCamera(simCam1);
-                reefCam1.setCamera(simCam1.getCam());
+        AprilTagCamSim simCam1 =
+                AprilTagCamSimBuilder.newCamera()
+                        .withCameraName("ScoreCam")
+                        .withTransform(camTrans1)
+                        .build();
+        aprilTagCamSim.addCamera(simCam1);
+        reefCam1.setCamera(simCam1.getCam());
 
-                AprilTagCamSim simCam2 =
-                        AprilTagCamSimBuilder.newCamera()
-                                .withCameraName("ClimbCam")
-                                .withTransform(camTrans2)
-                                .build();
-                aprilTagCamSim.addCamera(simCam2);
-                reefCam2.setCamera(simCam2.getCam());
+        AprilTagCamSim simCam2 =
+                AprilTagCamSimBuilder.newCamera()
+                        .withCameraName("ClimbCam")
+                        .withTransform(camTrans2)
+                        .build();
+        aprilTagCamSim.addCamera(simCam2);
+        reefCam2.setCamera(simCam2.getCam());
 
         limelight = new LimelightAprilTagSystem("limelight", drivetrain);
         climber = new ClimberSubsystem();
         coralManipulator = new CoralManipulatorSystem();
         mechanisms = new Mechanisms();
-        alignToReefCmd = null;
-                        new ReefAlignCommand(
-                                drivetrain,
-                                reefCam1,
-                                reefCam2,
-                                primaryXboxController::getLeftX,
-                                primaryXboxController::getLeftY,
-                                primaryXboxController::getRightX);
+        alignToReefCmd =
+        new ReefAlignCommand(
+                drivetrain,
+                reefCam1,
+                reefCam2,
+                primaryXboxController::getLeftX,
+                primaryXboxController::getLeftY,
+                primaryXboxController::getRightX);
 
         configureBindings();
 
@@ -175,9 +175,9 @@ public class RobotContainer {
         }
     }
 
-        public void updateVisionSim() {
-            aprilTagCamSim.update(drivetrain.getState().Pose);
-        }
+    public void updateVisionSim() {
+        aprilTagCamSim.update(drivetrain.getState().Pose);
+    }
 
     private void configureBindings() {
 
@@ -231,8 +231,8 @@ public class RobotContainer {
                 .leftTrigger()
                 .whileTrue(coralManipulator.grabber.transitionTo(GrabberState.ROLL_IN));
 
-                primaryXboxController.y().whileTrue(alignToReefCmd);
-                secondaryXboxController.start().onTrue(alignToReefCmd.toggleBranchSelection());
+        primaryXboxController.y().whileTrue(alignToReefCmd);
+        secondaryXboxController.start().onTrue(alignToReefCmd.toggleBranchSelection());
 
         secondaryXboxController.x().onTrue(coralManipulator.grabber.transitionTo(GrabberState.OFF));
         secondaryXboxController.leftBumper().whileTrue(climber.spinClimber(climber.climbSpeed));
--- src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
+++ src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "reefscape-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 387;
-    public static final String GIT_SHA = "40f91a7895f341f5e780b266f389bcccac80fdf8";
-    public static final String GIT_DATE = "2025-03-22 18:37:44 EDT";
+    public static final int GIT_REVISION = 388;
+    public static final String GIT_SHA = "fa9c2664d8fcdaeec309b759776e45cf963529af";
+    public static final String GIT_DATE = "2025-03-22 19:07:25 EDT";
     public static final String GIT_BRANCH = "event_meck";
-    public static final String BUILD_DATE = "2025-03-22 18:37:44 EDT";
-    public static final long BUILD_UNIX_TIME = 1742683064813L;
+    public static final String BUILD_DATE = "2025-03-22 19:07:26 EDT";
+    public static final long BUILD_UNIX_TIME = 1742684846167L;
     public static final int DIRTY = 0;
 
     private BuildConstants() {}
