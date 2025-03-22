55368db2f7e557b9629ab63775d343bb50af5a05
yetirobotics <yetiroboticsengineering@gmail.com>
2025-03-22 15:15:57 -0400

    Update at 'Sat Mar 22 15:15:56 EDT 2025'

--- src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
+++ src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "reefscape-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 377;
-    public static final String GIT_SHA = "c7356a757b6bc71c951153f659e556f8f72ae30e";
-    public static final String GIT_DATE = "2025-03-22 15:10:48 EDT";
+    public static final int GIT_REVISION = 378;
+    public static final String GIT_SHA = "827e257dff84229ada8a7dc2fe7e5439b66b9e43";
+    public static final String GIT_DATE = "2025-03-22 15:13:34 EDT";
     public static final String GIT_BRANCH = "event_meck";
-    public static final String BUILD_DATE = "2025-03-22 15:10:49 EDT";
-    public static final long BUILD_UNIX_TIME = 1742670649743L;
+    public static final String BUILD_DATE = "2025-03-22 15:13:35 EDT";
+    public static final long BUILD_UNIX_TIME = 1742670815734L;
     public static final int DIRTY = 0;
 
     private BuildConstants() {}
