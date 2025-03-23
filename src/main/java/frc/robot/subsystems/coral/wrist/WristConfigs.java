81e5cf4ba6f5eabcdc8e65cee30e91f38ce56d49
yetirobotics <yetiroboticsengineering@gmail.com>
2025-03-23 14:29:28 -0400

    Update at 'Sun Mar 23 14:29:28 EDT 2025'

--- src/main/java/frc/robot/subsystems/coral/wrist/WristConfigs.java
+++ src/main/java/frc/robot/subsystems/coral/wrist/WristConfigs.java
@@ -10,7 +10,7 @@ class WristConfigs {
     static final double WRIST_TOLERANCE = 0.1;
     static final int WRIST_KRAKEN_ID = 19;
     static final int WRIST_CANCODER_ID = 42;
-    static final double MAGNET_OFFSET = 0.223389 ;
+    static final double MAGNET_OFFSET = 0.223389;
 
     static final Slot0Configs SLOT_0_CONFIGS =
             new Slot0Configs()
--- src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
+++ src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "reefscape-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 397;
-    public static final String GIT_SHA = "1e52a41920c5feefa1ae9317c81b2658ffbcaa9c";
-    public static final String GIT_DATE = "2025-03-23 13:41:54 EDT";
+    public static final int GIT_REVISION = 398;
+    public static final String GIT_SHA = "fa643f1b1f607d6d7e040c7a6b23ee96ed8f32ac";
+    public static final String GIT_DATE = "2025-03-23 14:24:54 EDT";
     public static final String GIT_BRANCH = "event_meck";
-    public static final String BUILD_DATE = "2025-03-23 13:41:54 EDT";
-    public static final long BUILD_UNIX_TIME = 1742751714884L;
+    public static final String BUILD_DATE = "2025-03-23 14:24:55 EDT";
+    public static final long BUILD_UNIX_TIME = 1742754295072L;
     public static final int DIRTY = 0;
 
     private BuildConstants() {}
