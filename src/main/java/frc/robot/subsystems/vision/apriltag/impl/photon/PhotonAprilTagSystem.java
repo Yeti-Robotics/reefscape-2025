ad7bdc3413718fe17ee449332706d20a2d66c7de
yetirobotics <yetiroboticsengineering@gmail.com>
2025-03-22 22:11:59 -0400

    get rid of goofy photon epilogue logging

--- src/main/java/frc/robot/subsystems/vision/apriltag/impl/photon/PhotonAprilTagSystem.java
+++ src/main/java/frc/robot/subsystems/vision/apriltag/impl/photon/PhotonAprilTagSystem.java
@@ -27,24 +27,6 @@ public class PhotonAprilTagSystem extends SubsystemBase implements AprilTagSubsy
     private PhotonTrackedTarget currentBestDetection;
     private double currentBestDetectionTimestamp;
 
-//    @Logged(name = "TagPoses")
-//    public List<Pose2d> getTagPoses() {
-//        return aprilTagResults.getResults().stream()
-//                .map(AprilTagDetection::getRobotToTargetPose)
-//                .toList();
-//    }
-//
-//    @Logged(name = "Best Detection")
-//    public Pose2d getBestDetectionPose() {
-//        var bestDet = getBestDetection();
-//        return bestDet.map(AprilTagDetection::getRobotToTargetPose).orElse(null);
-//    }
-//
-//    @Logged(name = "Best Estimated Pose")
-//    public Pose2d logBestEstimatedPose() {
-//        return getEstimatedPose().map(AprilTagPose::getEstimatedRobotPose).orElse(null);
-//    }
-
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
     private Optional<EstimatedRobotPose> estimatedRobotPose = Optional.empty();
 
