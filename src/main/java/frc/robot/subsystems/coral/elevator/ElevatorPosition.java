4b056e521760619718d28b3b7790cbd7e029c330
yetirobotics <yetiroboticsengineering@gmail.com>
2025-03-23 15:28:44 -0400

    Update at 'Sun Mar 23 15:28:44 EDT 2025'

--- src/main/java/frc/robot/subsystems/coral/elevator/ElevatorPosition.java
+++ src/main/java/frc/robot/subsystems/coral/elevator/ElevatorPosition.java
@@ -9,7 +9,7 @@ public enum ElevatorPosition implements Comparable<ElevatorPosition> {
     POS_L1(2.4),
     POS_L2(0.367920),
     POS_L3(1.8),
-    POS_L4(3.8),
+    POS_L4(3.95),
     HOLD(-1); // special case, for when transitions are interrupted
 
     private final Angle height;
