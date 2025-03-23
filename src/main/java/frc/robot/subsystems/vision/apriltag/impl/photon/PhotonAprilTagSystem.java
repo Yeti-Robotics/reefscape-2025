908a28dbd613281b14cc15b06259bcc77451aa29
yetirobotics <yetiroboticsengineering@gmail.com>
2025-03-23 08:32:18 -0400

    formatting stuff

--- src/main/java/frc/robot/subsystems/vision/apriltag/impl/photon/PhotonAprilTagSystem.java
+++ src/main/java/frc/robot/subsystems/vision/apriltag/impl/photon/PhotonAprilTagSystem.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems.vision.apriltag.impl.photon;
 
-import edu.wpi.first.epilogue.Logged;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
--- src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
+++ src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "reefscape-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 388;
-    public static final String GIT_SHA = "fa9c2664d8fcdaeec309b759776e45cf963529af";
-    public static final String GIT_DATE = "2025-03-22 19:07:25 EDT";
+    public static final int GIT_REVISION = 391;
+    public static final String GIT_SHA = "ad7bdc3413718fe17ee449332706d20a2d66c7de";
+    public static final String GIT_DATE = "2025-03-22 22:11:59 EDT";
     public static final String GIT_BRANCH = "event_meck";
-    public static final String BUILD_DATE = "2025-03-22 19:07:26 EDT";
-    public static final long BUILD_UNIX_TIME = 1742684846167L;
+    public static final String BUILD_DATE = "2025-03-23 08:30:58 EDT";
+    public static final long BUILD_UNIX_TIME = 1742733058218L;
     public static final int DIRTY = 0;
 
     private BuildConstants() {}
