aca60eb5b195627508a3b384aad7b8fc9e503235
yetirobotics <yetiroboticsengineering@gmail.com>
2025-03-28 17:07:36 -0400

    fix stow issue

--- src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
+++ src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "reefscape-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 372;
-    public static final String GIT_SHA = "0f186b409643462646c32145162850455432e230";
-    public static final String GIT_DATE = "2025-03-27 19:57:45 EDT";
-    public static final String GIT_BRANCH = "transition-logic-rework";
-    public static final String BUILD_DATE = "2025-03-27 21:13:40 EDT";
-    public static final long BUILD_UNIX_TIME = 1743124420226L;
+    public static final int GIT_REVISION = 374;
+    public static final String GIT_SHA = "b67ab1b53a37057d9942bfd6257d9e88939b2416";
+    public static final String GIT_DATE = "2025-03-28 16:06:18 EDT";
+    public static final String GIT_BRANCH = "develop";
+    public static final String BUILD_DATE = "2025-03-28 17:00:23 EDT";
+    public static final long BUILD_UNIX_TIME = 1743195623436L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
