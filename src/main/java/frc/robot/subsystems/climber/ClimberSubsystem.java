bf2b9d2df924e48789a8b564b0fef0d26bf6803a
yetirobotics <yetiroboticsengineering@gmail.com>
2025-03-29 19:46:02 -0400

    auto align

--- src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -4,7 +4,6 @@ import static frc.robot.subsystems.climber.ClimberConfig.*;
 
 import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.CANcoder;
-import com.ctre.phoenix6.hardware.ParentDevice;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -27,7 +26,6 @@ public class ClimberSubsystem extends SubsystemBase {
 
         climber.getConfigurator().apply(climberTalonFXConfigs);
         cancoder.getConfigurator().apply(cancoderConfiguration);
-        ParentDevice.optimizeBusUtilizationForAll(climber, cancoder);
     }
 
     private void setClimberSpeed(double speed) {
--- src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
+++ src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "reefscape-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 394;
-    public static final String GIT_SHA = "fbd7733a08b563c911ed3b8cad9536f636a4a52f";
-    public static final String GIT_DATE = "2025-03-29 16:22:40 EDT";
+    public static final int GIT_REVISION = 409;
+    public static final String GIT_SHA = "123fe5170b789b5ef0015e557333431e26946f97";
+    public static final String GIT_DATE = "2025-03-29 19:22:07 EDT";
     public static final String GIT_BRANCH = "field-oriented-robot-autoalign";
-    public static final String BUILD_DATE = "2025-03-29 17:01:44 EDT";
-    public static final long BUILD_UNIX_TIME = 1743282104942L;
+    public static final String BUILD_DATE = "2025-03-29 19:26:27 EDT";
+    public static final long BUILD_UNIX_TIME = 1743290787005L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
