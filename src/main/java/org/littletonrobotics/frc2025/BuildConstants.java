85cd35b45b8d278ee6a121183aa0245ee0a80282
yetirobotics <yetiroboticsengineering@gmail.com>
2025-03-31 17:51:03 -0400

    fix L2 pos

--- src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
+++ src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "reefscape-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 409;
-    public static final String GIT_SHA = "123fe5170b789b5ef0015e557333431e26946f97";
-    public static final String GIT_DATE = "2025-03-29 19:22:07 EDT";
+    public static final int GIT_REVISION = 410;
+    public static final String GIT_SHA = "bf2b9d2df924e48789a8b564b0fef0d26bf6803a";
+    public static final String GIT_DATE = "2025-03-29 19:46:02 EDT";
     public static final String GIT_BRANCH = "field-oriented-robot-autoalign";
-    public static final String BUILD_DATE = "2025-03-29 19:26:27 EDT";
-    public static final long BUILD_UNIX_TIME = 1743290787005L;
+    public static final String BUILD_DATE = "2025-03-31 17:48:10 EDT";
+    public static final long BUILD_UNIX_TIME = 1743457690409L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
