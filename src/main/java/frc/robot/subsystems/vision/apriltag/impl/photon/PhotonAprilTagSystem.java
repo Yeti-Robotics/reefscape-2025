99cc3fd193069130c81e00cffecfce0d828f12a4
boomermath <boomermath13@gmail.com>
2025-03-31 20:32:23 -0400

    resolve merge

--- src/main/java/frc/robot/subsystems/vision/apriltag/impl/photon/PhotonAprilTagSystem.java
+++ src/main/java/frc/robot/subsystems/vision/apriltag/impl/photon/PhotonAprilTagSystem.java
@@ -42,7 +42,7 @@ public class PhotonAprilTagSystem extends SubsystemBase implements AprilTagSubsy
         this.drivetrain = commandSwerveDrivetrain;
 
         photonPoseEstimator.setMultiTagFallbackStrategy(
-                PhotonPoseEstimator.PoseStrategy.CLOSEST_TO_LAST_POSE);
+                PhotonPoseEstimator.PoseStrategy.LOWEST_AMBIGUITY);
     }
 
     @Override
--- src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
+++ src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "reefscape-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 386;
-    public static final String GIT_SHA = "91d8f2a3fc73e515f2fd0ea6aabeaab9af9736c6";
-    public static final String GIT_DATE = "2025-03-29 21:21:12 EDT";
-    public static final String GIT_BRANCH = "develop";
-    public static final String BUILD_DATE = "2025-03-31 17:57:37 EDT";
-    public static final long BUILD_UNIX_TIME = 1743458257586L;
+    public static final int GIT_REVISION = 419;
+    public static final String GIT_SHA = "578c3361a096c0183acc3b5f93ab457801e88b60";
+    public static final String GIT_DATE = "2025-03-31 20:02:02 EDT";
+    public static final String GIT_BRANCH = "field-oriented-autoalign-patch";
+    public static final String BUILD_DATE = "2025-03-31 20:29:09 EDT";
+    public static final long BUILD_UNIX_TIME = 1743467349284L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
