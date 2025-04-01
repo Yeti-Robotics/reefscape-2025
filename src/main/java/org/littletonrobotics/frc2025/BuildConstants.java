266f814e41744e05329429205a83813dfbc1c460
boomermath <boomermath13@gmail.com>
2025-03-31 20:32:23 -0400

    got drivecontroller working

--- src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
+++ src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "reefscape-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 418;
-    public static final String GIT_SHA = "fd1e23657186495ef1b4ebe988a93db5c2b15bed";
-    public static final String GIT_DATE = "2025-03-30 19:17:50 EDT";
-    public static final String GIT_BRANCH = "develop-autos";
-    public static final String BUILD_DATE = "2025-03-31 19:54:02 EDT";
-    public static final long BUILD_UNIX_TIME = 1743465242381L;
+    public static final int GIT_REVISION = 419;
+    public static final String GIT_SHA = "578c3361a096c0183acc3b5f93ab457801e88b60";
+    public static final String GIT_DATE = "2025-03-31 20:02:02 EDT";
+    public static final String GIT_BRANCH = "field-oriented-autoalign-patch";
+    public static final String BUILD_DATE = "2025-03-31 20:29:09 EDT";
+    public static final long BUILD_UNIX_TIME = 1743467349284L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
