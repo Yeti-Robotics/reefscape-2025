d23e00a630ca1865340b2a7a102f1a126eab5d8e
Taran Duba <taran.duba3@gmail.com>
2025-04-02 19:51:19 -0400

    tuned rgb values for nick mode and algae color

--- src/main/java/frc/robot/subsystems/led/LEDConstants.java
+++ src/main/java/frc/robot/subsystems/led/LEDConstants.java
@@ -7,11 +7,11 @@ import edu.wpi.first.wpilibj.util.Color;
 
 public final class LEDConstants {
     public static final int LED_STRIP_PORT = 0;
-    public static final int LED_COUNT = 36;
+    public static final int LED_COUNT = 72;
     public static final Distance LED_SPACING = Meters.of(1 / 65.0);
     public static final Color YETI_BLUE = new Color(84, 182, 229);
-    public static final Color ALGAE_COLOR = new Color(79, 181, 165);
-    public static final Color NICK_ORANGE = new Color(240, 119, 99);
+    public static final Color ALGAE_COLOR = new Color(79, 90, 255);
+    public static final Color NICK_ORANGE = new Color(255, 0, 50);
     public static final double ZERO_TOLERANCE = 0.005;
     private static final int PROGRESS_PARTS = 4;
     public static final double PROGRESS_INCREMENT = 1.0 / PROGRESS_PARTS;
