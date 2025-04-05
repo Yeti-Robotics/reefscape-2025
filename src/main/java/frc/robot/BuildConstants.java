84394503e9447be19560381ffa4448167561af29
yetirobotics <yetiroboticsengineering@gmail.com>
2025-04-05 15:47:24 -0400

    Update at 'Sat Apr 05 15:47:24 EDT 2025'

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "reefscape-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 476;
-    public static final String GIT_SHA = "79afe7a4e71a447551670849f1cd598221d145c4";
-    public static final String GIT_DATE = "2025-04-05 11:38:04 EDT";
+    public static final int GIT_REVISION = 477;
+    public static final String GIT_SHA = "0e52cbe434682299a98b42f5d5ceda866777975b";
+    public static final String GIT_DATE = "2025-04-05 15:46:18 EDT";
     public static final String GIT_BRANCH = "event_dcmp";
-    public static final String BUILD_DATE = "2025-04-05 12:48:10 EDT";
-    public static final long BUILD_UNIX_TIME = 1743871690968L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2025-04-05 15:46:19 EDT";
+    public static final long BUILD_UNIX_TIME = 1743882379905L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
--- src/main/java/frc/robot/subsystems/coral/CoralManipulatorSystem.java
+++ src/main/java/frc/robot/subsystems/coral/CoralManipulatorSystem.java
@@ -12,7 +12,6 @@ import frc.robot.subsystems.coral.grabber.GrabberSubsystem;
 import frc.robot.subsystems.coral.wrist.WristPositions;
 import frc.robot.subsystems.coral.wrist.WristSubsystem;
 import frc.robot.util.state.StatefulSubsystem;
-
 import java.util.Map;
 
 @Logged
@@ -117,11 +116,11 @@ public class CoralManipulatorSystem extends StatefulSubsystem<CoralManipulatorSt
 
     public boolean isIntaking(CoralManipulatorState targetState) {
         return (targetState == CoralManipulatorState.HP_INTAKE
-                || targetState == CoralManipulatorState.GROUND_INTAKE
-                && getCurrentState() == CoralManipulatorState.STOWED)
+                        || targetState == CoralManipulatorState.GROUND_INTAKE
+                                && getCurrentState() == CoralManipulatorState.STOWED)
                 || (targetState == CoralManipulatorState.STOWED
-                && getCurrentState() == CoralManipulatorState.HP_INTAKE
-                || getCurrentState() == CoralManipulatorState.GROUND_INTAKE);
+                                && getCurrentState() == CoralManipulatorState.HP_INTAKE
+                        || getCurrentState() == CoralManipulatorState.GROUND_INTAKE);
     }
 
     public void queueState(CoralManipulatorState state) {
@@ -169,8 +168,8 @@ public class CoralManipulatorSystem extends StatefulSubsystem<CoralManipulatorSt
                                     .andThen(grabber.transitionTo(targetState.getGrabberState()));
                 } else {
                     coralManipulatorCommand =
-                            grabber.transitionTo(targetState.getGrabberState()).alongWith(
-                                            arm.transitionTo(targetState.getArmPosition()))
+                            grabber.transitionTo(targetState.getGrabberState())
+                                    .alongWith(arm.transitionTo(targetState.getArmPosition()))
                                     .alongWith(
                                             elevator.transitionTo(
                                                     targetState.getElevatorPosition()));
