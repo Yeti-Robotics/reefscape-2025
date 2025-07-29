58e6f9b0744afc1dff066d2bd9b763dff3d2ace7
Taran Duba <taran.duba3@gmail.com>
2025-07-29 06:26:36 -0400

    trying to work out new update function

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "reefscape-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 579;
-    public static final String GIT_SHA = "2dc620c66a2f6e87cc0b0c19d33d00c1d3524b3e";
-    public static final String GIT_DATE = "2025-04-13 23:52:30 EDT";
+    public static final int GIT_REVISION = 580;
+    public static final String GIT_SHA = "089dbdddda732f9e811b4146f1acac41dae080f7";
+    public static final String GIT_DATE = "2025-07-23 13:21:04 EDT";
     public static final String GIT_BRANCH = "new-auto-align-dev";
-    public static final String BUILD_DATE = "2025-07-23 11:03:49 EDT";
-    public static final long BUILD_UNIX_TIME = 1753283029236L;
+    public static final String BUILD_DATE = "2025-07-28 16:33:01 EDT";
+    public static final long BUILD_UNIX_TIME = 1753734781364L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
--- src/main/java/frc/robot/util/sim/vision/AprilTagSimulator.java
+++ src/main/java/frc/robot/util/sim/vision/AprilTagSimulator.java
@@ -3,6 +3,13 @@ package frc.robot.util.sim.vision;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.vision.apriltag.AprilTagDetection;
+import frc.robot.subsystems.vision.apriltag.AprilTagResults;
 import java.util.ArrayList;
 import java.util.List;
 import org.photonvision.simulation.VisionSystemSim;
@@ -23,16 +30,34 @@ public class AprilTagSimulator {
         aprilTagCamSims.add(aprilTagCamSim);
     }
 
-    /* <<<<<<<<<<<<<<  ✨ Windsurf Command ⭐ >>>>>>>>>>>>>>>> */
     /**
      * Update the simulator with the current pose of the robot.
      *
      * @param pose The current pose of the robot.
      */
-    /* <<<<<<<<<<  07c48757-5cd3-4ab9-9339-a50b85422bce  >>>>>>>>>>> */
     public void update(Pose2d pose) {
         visionSim.update(pose);
-        aprilTagCamSims.forEach(AprilTagCamSim::publishSeenTags);
+        
+        for (AprilTagCamSim camSim : aprilTagCamSims) {
+            var results = camSim.getCam().getAllUnreadResults();
+            if (!results.isEmpty()) {
+                List<AprilTagDetection> detections = new ArrayList<>();
+                for (var target : results.get(0).getTargets()) {
+                    Transform3d cameraToTarget = target.getBestCameraToTarget();
+                    Pose2d targetPose = new Pose2d(
+                        cameraToTarget.getX(),
+                        cameraToTarget.getY(),
+                        cameraToTarget.getRotation().toRotation2d()
+                    );
+                    detections.add(new AprilTagDetection(target.getFiducialId(), pose, targetPose, target.getPoseAmbiguity()));
+                }
+                
+                AprilTagResults aprilTagResults = new AprilTagResults(Timer.getFPGATimestamp(), 20, detections);
+                List<Pose3d> seenTags = AprilTagCamSim.publishSeenTags(aprilTagResults);
+                
+                seenTags.forEach(camSim::publishSeenTags);
+            }
+        }
     }
 
     public List<AprilTagCamSim> getAprilTagCamSims() {
