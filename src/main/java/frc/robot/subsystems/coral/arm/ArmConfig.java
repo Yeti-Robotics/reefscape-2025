ff14a13690e35e56b4aaf54a7fff941720231865
yetirobotics <yetiroboticsengineering@gmail.com>
2025-08-16 17:15:35 -0400

    Fixed arm mag offset

--- src/main/java/frc/robot/subsystems/coral/arm/ArmConfig.java
+++ src/main/java/frc/robot/subsystems/coral/arm/ArmConfig.java
@@ -9,7 +9,7 @@ class ArmConfig {
     static final int ARM_KRAKEN_ID = 10;
     static final int ARM_CANCODER_ID = 0;
 
-    static final double MAGNET_OFFSET = -0.247314;
+    static final double MAGNET_OFFSET = -0.325928;
     static final double GEAR_RATIO = 75.6055;
 
     static final double ARM_DEPLOY_LOWER_BOUND = 0;
