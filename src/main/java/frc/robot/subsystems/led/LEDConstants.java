065397afc7231e7af53a4af5ce6dfbc0103affcf
yetirobotics <yetiroboticsengineering@gmail.com>
2025-08-21 15:48:04 -0400

    reallocated led strip port

--- src/main/java/frc/robot/subsystems/led/LEDConstants.java
+++ src/main/java/frc/robot/subsystems/led/LEDConstants.java
@@ -6,7 +6,7 @@ import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.util.Color;
 
 public final class LEDConstants {
-    public static final int LED_STRIP_PORT = 0;
+    public static final int LED_STRIP_PORT = 3;
     public static final int LED_COUNT = 72;
     public static final Distance LED_SPACING = Meters.of(1 / 65.0);
     public static final Color YETI_BLUE = new Color(84, 182, 229);
