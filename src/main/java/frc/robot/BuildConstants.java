c96cc579c9b3516556abaff985297c35a7c71be5
yetirobotics <yetiroboticsengineering@gmail.com>
2025-09-27 13:28:05 -0400

    wrist 0

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "reefscape-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 590;
-    public static final String GIT_SHA = "661474b2c80d55138228008f78d463556f49abf1";
-    public static final String GIT_DATE = "2025-09-25 19:27:06 EDT";
+    public static final int GIT_REVISION = 591;
+    public static final String GIT_SHA = "1eab967923c075cf82cc06db13bc8769fd443ad1";
+    public static final String GIT_DATE = "2025-09-25 20:59:29 EDT";
     public static final String GIT_BRANCH = "flossy_rio_fromdevelop";
-    public static final String BUILD_DATE = "2025-09-25 19:34:28 EDT";
-    public static final long BUILD_UNIX_TIME = 1758843268649L;
+    public static final String BUILD_DATE = "2025-09-27 12:55:42 EDT";
+    public static final long BUILD_UNIX_TIME = 1758992142470L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
